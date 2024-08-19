55f7c43889d6131219dfae2977f82c56b9e33b25
Zander Lewis <zander@zanderlewis.dev>
2024-08-19 16:53:55 -0400

    Better Comments

--- calculator.f90
+++ calculator.f90
@@ -8,16 +8,19 @@ program calculator
     character(len=*), parameter :: red = char(27) // '[31m'
     character(len=*), parameter :: yellow = char(27) // '[33m'
     character(len=*), parameter :: green = char(27) // '[32m'
-    ! End of variable definition
+    ! END
 
     ! Main program
+    ! Print instructions and read input
     print *, yellow // 'Enter first number: ' // reset
     read *, num1
     print *, yellow // 'Enter operator (+, -, *, /): ' // reset
     read *, operator
     print *, yellow // 'Enter second number: ' // reset
     read *, num2
+    ! END
 
+    ! Perform calculation
     if (operator == '+') then
         result = num1 + num2
     else if (operator == '-') then
@@ -26,6 +29,7 @@ program calculator
         result = num1 * num2
     else if (operator == '/') then
         if (num2 == 0) then
+            ! Prevent division by zero
             print *, red // 'Division by zero is prohibited' // reset
             stop
         end if
@@ -34,8 +38,10 @@ program calculator
         print *, red // 'Invalid operator' // reset
         stop
     end if
+    ! END
 
+    ! Print result
     write(result_str, '(F6.2)') result
     print *, green // 'Result: ' // trim(result_str) // reset
-    ! End of main program
+    ! END
 end program calculator
